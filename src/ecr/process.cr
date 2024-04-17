7e964a572963dd7a270b31111ad81e7909a777c0
Johannes Müller <straightshoota@gmail.com>
2024-04-17 12:32:47 +0200

    Backtrace in `ecr/process`

--- src/ecr/process.cr
+++ src/ecr/process.cr
@@ -6,6 +6,7 @@ buffer_name = ARGV[1]
 begin
   puts ECR.process_file(filename, buffer_name)
 rescue ex : File::Error
-  STDERR.puts ex.message
+  STDERR.puts "Error in ECR processor:"
+  ex.inspect_with_backtrace(STDERR)
   exit 1
 end
